e23d62cefa53c1e743eab3cc8a62ec30634d63ce
Mas RJ95 <48236174+maslanangdev@users.noreply.github.com>
2025-10-27 09:03:28 +0700

    add demo loader

---
+++ Safe Step Game/Assets/Script/DemoLoader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DemoLoader : MonoBehaviour
+{
+    public static DemoLoader instance;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    
+}
