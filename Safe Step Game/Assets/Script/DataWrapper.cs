bad453fa2f8ddb51979e1693b8895359d84fabfd
Subekti <daffahidayatulah123@gmail.com>
2025-10-27 11:52:49 +0700

    fix bug tidak bisa build android

--- Safe Step Game/Assets/Script/DataWrapper.cs
+++ Safe Step Game/Assets/Script/DataWrapper.cs
@@ -6,7 +6,7 @@ public class DataWrapper : MonoBehaviour
 {
 
     DataWrapper instance;
-
+#if UNITY_WEBGL && !UNITY_EDITOR
     [DllImport("__Internal")]
     public static extern void HelloWorld();
 
@@ -21,11 +21,14 @@ public class DataWrapper : MonoBehaviour
 
     [DllImport("__Internal")]
     public static extern void ClearLocalStorage();
+#endif
 
 
     void Start()
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
         HelloWorld();
+#endif
     }
 
     void Awake()
